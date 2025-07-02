20e7a0a5100076346b747b709315f931f0306224
JNico <nicoolano43@gmail.com>
2025-07-02 22:20:06 +0800

    frontend: display search results

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,34 @@
+import { useState, type FormEvent } from "react";
 import "./App.css";
+import * as api from "./api";
+import type { Recipe } from "./types";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("pizza");
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+  const handleSearchSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    try {
+      const recipes = await api.searchRecipes(searchTerm, 1);
+      setRecipes(recipes.results);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   return (
     <div>
-      Hello from Recipe App!
+      <form onSubmit={(event)=> handleSearchSubmit(event)}>
+        <button type="submit">Submit</button>
+      </form>
+      
+      {recipes.map((recipe) => (
+        <div>
+          recipe image location: {recipe.image}
+          recipe tite: {recipe.title}
+        </div>
+      ))}
     </div>
   )
 }
