a22e1c75dd941c209d8af331c6b7ad3e3f1e6f09
JNico <nicoolano43@gmail.com>
2025-07-05 14:17:58 +0800

    frontend: Search input and Recipe Card Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,10 @@ import { useState, type FormEvent } from "react";
 import "./App.css";
 import * as api from "./api";
 import type { Recipe } from "./types";
+import RecipeCard from "./components/RecipeCard";
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState("pizza");
+  const [searchTerm, setSearchTerm] = useState<string>("");
   const [recipes, setRecipes] = useState<Recipe[]>([]);
 
   const handleSearchSubmit = async (event: FormEvent) => {
@@ -20,14 +21,19 @@ const App = () => {
   return (
     <div>
       <form onSubmit={(event)=> handleSearchSubmit(event)}>
+        <input 
+          type="text" 
+          required 
+          placeholder="Enter a search term ..."
+          value={searchTerm}
+          onChange={(event)=> setSearchTerm(event.target.value)}
+          >
+        </input>
         <button type="submit">Submit</button>
       </form>
       
       {recipes.map((recipe) => (
-        <div>
-          recipe image location: {recipe.image}
-          recipe tite: {recipe.title}
-        </div>
+        <RecipeCard recipe={recipe}/>
       ))}
     </div>
   )
