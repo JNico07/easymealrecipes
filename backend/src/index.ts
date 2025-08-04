e23f39bb74a9a4abdb133e2e23e0efc11659e355
JNico <nicoolano43@gmail.com>
2025-08-04 23:24:36 +0800

    fix: update CORS origin to allow multiple domains

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,7 +17,10 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://recipe-app-production-39fc.up.railway.app",
+    ],
     credentials: true, // allow cookies
   })
 );
