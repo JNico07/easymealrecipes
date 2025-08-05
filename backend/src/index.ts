7179581289ddb71333408cccbb5e59ae2c8be49c
JNico <nicoolano43@gmail.com>
2025-08-05 15:19:16 +0800

    update backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -285,8 +285,6 @@ app.delete("/api/recipes/favourite", async (req, res) => {
   }
 });
 
-const PORT = Number(process.env.PORT) || 5000;
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(5000, () => {
+  console.log("Server is running on localhost:5000");
 });
